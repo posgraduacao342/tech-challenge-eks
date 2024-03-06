baf90d3ab99e659c2bf2eb46e39ecaa8ced4717f
posgraduacao342 <149441051+posgraduacao342@users.noreply.github.com>
2024-03-06 11:34:06 -0300

    including workflow dispatch on main.yml (#5)
    * including workflow dispatch on main.yml
    
    * Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "pos-graduacao-fiap-terraform-state-us-east1"
+    bucket = "pos-graduacao-fiap-terraform-state"
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
